3f6130c7fbbaf3ed51f4f9955514fa82b3c2da14
jaygil8755 <68841210+jaygil8755@users.noreply.github.com>
2023-08-11 16:27:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 from rembg import remove
 from PIL import Image
-
+from streamlit_image_comparison import image_comparison
 st.subheader('이미지 배경제거 서비스')
-
+st.markdown("## sample result")
+image_comparison(
+    img1 = ""
+    img2 = ""
+    label1 = "원본 이미지"
+    label2 = "배경제거 이미지"
 uploaded_file = st.file_uploader("이미지를 업로드하세요", type=['png', 'jpg', 'jpeg'])
 
 if uploaded_file is not None:
