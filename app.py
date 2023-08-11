26510862a187155e448f4f429e1e24e197bf9ca9
jaygil8755 <68841210+jaygil8755@users.noreply.github.com>
2023-08-11 16:33:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from streamlit_image_comparison import image_comparison
 st.subheader('이미지 배경제거 서비스')
 st.markdown("## sample result")
 image_comparison(
-    img1 = ""
-    img2 = ""
+    img1 = "https://raw.githubusercontent.com/jaygil8755/first_app/3ccd1f9edd28745c8c7f63f1c839f87d29d46ab8/src/animal-1.jpg"
+    img2 = "https://raw.githubusercontent.com/jaygil8755/first_app/3ccd1f9edd28745c8c7f63f1c839f87d29d46ab8/src/animal_rmbg.png"
     label1 = "원본 이미지"
-    label2 = "배경제거 이미지"
+    label2 = "배경제거 이미지")
+
 uploaded_file = st.file_uploader("이미지를 업로드하세요", type=['png', 'jpg', 'jpeg'])
 
 if uploaded_file is not None:
