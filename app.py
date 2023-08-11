043315ee6d4375a6214738962477177075d7cef3
jaygil8755 <68841210+jaygil8755@users.noreply.github.com>
2023-08-11 17:55:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-# import streamlit as st
-# from rembg import remove
-# from PIL import Image
-# from streamlit_image_comparison import image_comparison
-# import easyocr as ocr  #OCR
-# import numpy as np
+import streamlit as st
+from rembg import remove
+from PIL import Image
+from streamlit_image_comparison import image_comparison
+import easyocr as ocr  #OCR
+import numpy as np
 
-# # set page config
-# st.set_page_config(layout="centered")
+# set page config
+st.set_page_config(layout="centered")
 
-# st.subheader('Remove Background - 사진 배경 제거')
-# st.markdown("#### sample result")
-# image_comparison(
-#     img1 = "https://raw.githubusercontent.com/jaygil8755/first_app/3ccd1f9edd28745c8c7f63f1c839f87d29d46ab8/src/animal-1.jpg",
-#     img2 = "https://raw.githubusercontent.com/jaygil8755/first_app/3ccd1f9edd28745c8c7f63f1c839f87d29d46ab8/src/animal_rmbg.png",
-#     label1 = "원본 이미지",
-#     label2 = "배경제거 이미지",
-#     show_labels=True,
-#     make_responsive=True,
-#     in_memory=True)
+st.subheader('Remove Background - 사진 배경 제거')
+st.markdown("#### sample result")
+image_comparison(
+    img1 = "https://raw.githubusercontent.com/jaygil8755/first_app/3ccd1f9edd28745c8c7f63f1c839f87d29d46ab8/src/animal-1.jpg",
+    img2 = "https://raw.githubusercontent.com/jaygil8755/first_app/3ccd1f9edd28745c8c7f63f1c839f87d29d46ab8/src/animal_rmbg.png",
+    label1 = "원본 이미지",
+    label2 = "배경제거 이미지",
+    show_labels=True,
+    make_responsive=True,
+    in_memory=True)
 
 # uploaded_file = st.file_uploader("이미지를 업로드하세요", type=['png', 'jpg', 'jpeg'])
 
@@ -65,7 +65,7 @@ image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpe
 
 @st.cache
 def load_model(): 
-    reader = ocr.Reader(['en'],model_storage_directory='.')
+    reader = ocr.Reader(['ko', 'en'],model_storage_directory='.')
     return reader 
 
 reader = load_model() #load model
